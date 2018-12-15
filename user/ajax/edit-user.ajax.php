4e9580b96913307c4f831eb5f143acf3c10518a6
d3f4ult0 <addh2.0.1@gmail.com>
2018-12-15 15:15:31 -0600

    Reparaciones minimas al user

--- user/ajax/edit-user.ajax.php
+++ user/ajax/edit-user.ajax.php
@@ -12,17 +12,19 @@ $answerJSON = array('registerMes' => "", 'registerNum' => 0, 'registerUser' => 0
 if ($_POST['id'] != "") {
   $id = $_POST['id'];
   $name = $_POST['name'];
+  $genero = $_POST['genero'];
   $phone = $_POST['phone'];
   $address = $_POST['address'];
   $curp = $_POST['curp'];
   $rfc = $_POST['rfc'];
 
   $ver = $connection->prepare('UPDATE usuario
-  SET nombre = :name, telefono = :phone, direccion = :address, curp = :curp, rfc = :rfc
+  SET nombre = :name, genero = :genero, telefono = :phone, direccion = :address, curp = :curp, rfc = :rfc
   WHERE id_usuario = :id');
   $ver->execute(array(
     ':id' => $id,
     ':name' => $name,
+    ':genero' =>$genero,
     ':phone' => $phone,
     ':address' => $address,
     ':curp' => $curp,
