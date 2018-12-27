8d689d8108309860791c93c2e18a0ce88c3c511b
d3f4ult0 <addh2.0.1@gmail.com>
2018-12-27 11:14:55 -0600

    Prueba de script

--- user/ajax/new-estatus.ajax.php
+++ user/ajax/new-estatus.ajax.php
@@ -12,21 +12,50 @@ $answerJSON = array('check' => 0, 'num' => 0, 'error' => "");
 if ($_GET['tipo'] != "") {
   $tipo = $_GET['tipo'];
 
-  $num = $connection->prepare('SELECT * FROM estatus');
-  $num->execute();
-  $num->fetchAll();
-  // $numSig = $numSig[0];
-  echo $num;
-  // if ($numSig["id_estatus"]>0) {
-  //   $answerJSON['num'] = $numSig["id_estatus"] + 1;
-  //   $answerJSON['check'] = 1;
-  // } else {
-  //   $answerJSON['error'] = "Problema interno del servidor 1";
-  //   $answerJSON['check'] = 0;
-  // }
+  $gen = $connection->prepare('SELECT * FROM estatus WHERE tipo="'.$tipo.'" ORDER BY id_estatus DESC LIMIT 1');
+  $gen->execute();
+  print_r($gen);
+  if ($gen["id_estatus"]>0) {
+    $answerJSON['num'] = $gen["id_estatus"] + 1;
+    $answerJSON['check'] = 1;
+  } else {
+    $answerJSON['error'] = "Problema interno del servidor 1";
+    $answerJSON['check'] = 0;
+  }
 }else {
   $answerJSON['error'] = "Problema interno del servidor 2";
   $answerJSON['check'] = 0;
+}
+echo json_encode($answerJSON);
+?>
+
+<?php
+require '../../config/config.php';
+require '../../config/functions.php';
+
+$connection = dbConnection($db_config);
+if (!$connection) {
+  header('Location: error.php');
+}
+
+$answerJSON = array('registerMes' => "", 'registerNum' => 0);
+
+if ($_POST['pass'] != "" && $_POST['id'] != "") {
+  $pass = $_POST['pass'];
+  $pass = token($pass);
+  $id = $_POST['id'];
+
+  $ver = $connection->prepare('UPDATE usuario
+  SET password = :pass
+  WHERE id_usuario = :id');
+  $ver->execute(array(':pass' => $pass, ':id' => $id));
+  $print = getDataFromWhere($connection,"usuario","id_usuario",$id);
+  $print = $print[0];
+    $answerJSON['registerMes'] = "¡Contraseña cambiada con exito!";
+    $answerJSON['registerNum'] = $print['id_usuario'];
+}else {
+  $answerJSON['registerNum'] = 0;
+  $answerJSON['registerMes'] = "Falla en el Servidor 01 :(";
 }
 echo json_encode($answerJSON);
 ?>
