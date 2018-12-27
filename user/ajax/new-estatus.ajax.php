1baad88452db1b1f7cc72d05514539696ced4193
d3f4ult0 <addh2.0.1@gmail.com>
2018-12-27 00:46:27 -0600

    Reparacion del script

--- user/ajax/new-estatus.ajax.php
+++ user/ajax/new-estatus.ajax.php
@@ -14,16 +14,16 @@ if ($_GET['tipo'] != "") {
 
   $numSig = $connection->prepare('SELECT * FROM estatus WHERE tipo="U" ORDER BY id_estatus DESC LIMIT 1');
   $numSig->execute();
-  $numSig->fetch();
-
-  print_r ($numSig);
-  // if ($numSig["id_estatus"]>0) {
-  //   $answerJSON['num'] = $numSig["id_estatus"] + 1;
-  //   $answerJSON['check'] = 1;
-  // } else {
-  //   $answerJSON['error'] = "Problema interno del servidor 1";
-  //   $answerJSON['check'] = 0;
-  // }
+  $numSig->fetchAll();
+  $numSig = $numSig[0];
+  // print_r ($numSig);
+  if ($numSig["id_estatus"]>0) {
+    $answerJSON['num'] = $numSig["id_estatus"] + 1;
+    $answerJSON['check'] = 1;
+  } else {
+    $answerJSON['error'] = "Problema interno del servidor 1";
+    $answerJSON['check'] = 0;
+  }
 }else {
   $answerJSON['error'] = "Problema interno del servidor 2";
   $answerJSON['check'] = 0;
