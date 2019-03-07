7c3ad7be4630dbfe8200281b43e7042186e2f37d
d3f4ult0 <addh2.0.1@gmail.com>
2019-03-06 21:58:30 -0600

    Prueba de bostrap4 en index

---
+++ view/index.view.php
@@ -0,0 +1,55 @@
+<div class="jumbotron main">
+   <div class="container">
+      <h1 class="text-center">Nueva session 0.1.3</h1>
+      <h2>Se va a empezar a trabaja una nueva sesion para el usuario y administrador.</h2>
+      <h3>Se va a conservar los estilos CSS, solo se va a cambiar la programacion de js para manejarla como se tenia anterior mente, solo se conservaran las verificaciones con el metodo blur()</h3>
+      <h5 class="text-right">DefaultCORP© Abril-2017</h5>
+   </div>
+</div>
+<div class="container">
+   <div class="col-xs-12 col-md-9">
+      <h4>Puntos a trabajar:</h4>
+         <ol>
+            <li>Cabecera y pie de pagina se conservaran <span class="glyphicon glyphicon-ok verde"></span></li>
+            <li>Aside dinamico para el cambio automatico <span class="glyphicon glyphicon-ok verde"></span></li>
+            <li>Registro de usuario en new-user.php <span class="glyphicon glyphicon-ok verde"></span>
+               <ul>
+                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
+               </ul>
+            </li>
+            <li>Verificar correo del usuario en check.php <span class="glyphicon glyphicon-ok verde"></span>
+               <ul>
+                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
+               </ul>
+            </li>
+            <li>Pedir restauracion de contraseña en restore.php <span class="glyphicon glyphicon-ok verde"></span>
+               <ul>
+                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
+               </ul>
+            </li>
+            <li>Restaurar la contraseña olvidada en reset-password.php <span class="glyphicon glyphicon-ok verde"></span>
+               <ul>
+                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
+               </ul>
+            </li>
+            <li>Inicio de sesion en login.php <span class="glyphicon glyphicon-ok verde"></span>
+               <ul>
+                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
+                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
+               </ul>
+            </li>
+         </ol>
+   </div>
+   <?php require 'views/aside.php'; ?>
+</div>
+
+<?php require 'views/footer.php'; ?>
