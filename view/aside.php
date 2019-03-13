67f732283ae10cc1b2b9d501bcd0f468f918522a
d3f4ult0 <addh2.0.1@gmail.com>
2019-03-12 20:40:48 -0600

    Modificación del aside

--- view/aside.php
+++ view/aside.php
@@ -1,17 +1,22 @@
-<aside class="col-xs-12 col-md-3">
-   <h3 class="text-center menu-aside">Menú de navegación</h3>
-   <div class="list-group">
-      <a href="index.php" class="list-group-item <?php if ($pagina == "index.php"): ?>
-         active
-      <?php endif; ?>"><span class="glyphicon glyphicon-home"></span> Inicio</a>
-      <a href="new-user.php" class="list-group-item <?php if ($pagina == "new-user.php"): ?>
-         active
-      <?php endif; ?>"><span class="glyphicon glyphicon-ice-lolly-tasted"></span> Registrate</a>
-      <a href="login.php" class="list-group-item <?php if ($pagina == "login.php"): ?>
-         active
-      <?php endif; ?>"><span class="glyphicon glyphicon-user"></span> Iniciar sesion</a>
-      <a href="restore.php" class="list-group-item <?php if ($pagina == "restore.php"): ?>
-         active
-      <?php endif; ?>"><span class="glyphicon glyphicon-lock"></span> Contraseña Olvidada</a>
-   </div>
+<aside class="col-12 col-md-3">
+  <div class="list-group list-group-flush menu-aside">
+    <a href="index.php" class="list-group-item list-group-item-action <?php if ($page == "index.php") {
+        echo "active";
+      } ?>"><i class="material-icons">home</i> Inicio</a>
+    <a href="quienes.php" class="list-group-item list-group-item-action <?php if ($page == "quienes.php") {
+        echo "active";
+      } ?>"><i class="material-icons">contact_support</i> ¿Quienes somos?</a>
+    <a href="contacto.php" class="list-group-item list-group-item-action <?php if ($page == "contacto.php") {
+        echo "active";
+      } ?>"><i class="material-icons">forum</i> Contacto</a>
+    <a href="new-user.php" class="list-group-item list-group-item-action <?php if ($page == "new-user.php") {
+        echo "active";
+      } ?>"><i class="material-icons">person_add</i> Registrate</a>
+    <a href="login.php" class="list-group-item list-group-item-action <?php if ($page == "login.php") {
+        echo "active";
+      } ?>"><i class="material-icons">person</i> Portal del usuario</a>
+    <a href="restore.php" class="list-group-item list-group-item-action <?php if ($page == "restore.php") {
+        echo "active";
+      } ?>"><i class="material-icons">lock</i> ¿Contraseña Olvidada?</a>
+  </div>
 </aside>
