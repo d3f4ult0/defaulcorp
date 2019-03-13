d833ede97e3f6d2793143b3737528ef40158021e
d3f4ult0 <addh2.0.1@gmail.com>
2019-03-12 20:29:12 -0600

    Edicion del index y aside prueba

--- view/index.view.php
+++ view/index.view.php
@@ -1,57 +1,17 @@
 <div class="jumbotron main">
    <div class="container">
-      <h1 class="text-center">Nueva session 0.1.3</h1>
-      <h2>Se va a empezar a trabaja una nueva sesion para el usuario y administrador.</h2>
-      <h3>Se va a conservar los estilos CSS, solo se va a cambiar la programacion de js para manejarla como se tenia anterior mente, solo se conservaran las verificaciones con el metodo blur()</h3>
-      <h5 class="text-right">DefaultCORP© Abril-2017</h5>
+      <h1 class="text-center">Nueva session para TuPsique 0.1.3 (Aun a discusion con el team)</h1>
+      <h3>Platicarlo con el team sobre lo que tiene que contener la pagina principal</h3>
    </div>
 </div>
+
 <div class="container">
-   <div class="col-xs-12 col-md-9">
-      <h4>Puntos a trabajar:</h4>
-      <ol>
-        <li>Prueba de push con gitKraken<span></span></li>
-        <li><h1>Prueba exitosa!</h1><span></span></li>
-      </ol>
-         <ol>
-            <li>Cabecera y pie de pagina se conservaran <span class="glyphicon glyphicon-ok verde"></span></li>
-            <li>Aside dinamico para el cambio automatico <span class="glyphicon glyphicon-ok verde"></span></li>
-            <li>Registro de usuario en new-user.php <span class="glyphicon glyphicon-ok verde"></span>
-               <ul>
-                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
-               </ul>
-            </li>
-            <li>Verificar correo del usuario en check.php <span class="glyphicon glyphicon-ok verde"></span>
-               <ul>
-                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
-               </ul>
-            </li>
-            <li>Pedir restauracion de contraseña en restore.php <span class="glyphicon glyphicon-ok verde"></span>
-               <ul>
-                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
-               </ul>
-            </li>
-            <li>Restaurar la contraseña olvidada en reset-password.php <span class="glyphicon glyphicon-ok verde"></span>
-               <ul>
-                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
-               </ul>
-            </li>
-            <li>Inicio de sesion en login.php <span class="glyphicon glyphicon-ok verde"></span>
-               <ul>
-                  <li>Vista <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Logica <span class="glyphicon glyphicon-ok verde"></span></li>
-                  <li>Script <span class="glyphicon glyphicon-ok verde"></span></li>
-               </ul>
-            </li>
-         </ol>
-   </div>
-   <?php require 'view/aside.php'; ?>
+  <div class="row">
+    <div class="col-12 col-md-9">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolores dignissimos vel perspiciatis ut laboriosam placeat sapiente assumenda labore vero quibusdam aliquam iste impedit quaerat, animi, voluptas deserunt alias. Dicta.</span>
+      <span>Suscipit recusandae animi blanditiis est, mollitia, quidem sed at excepturi fugiat dolor eos quae accusantium libero sit error unde? Necessitatibus qui pariatur cum quae, voluptate amet nostrum odio, voluptates at!</span>
+      <span>Commodi repellendus perferendis obcaecati accusantium, necessitatibus sunt quisquam, consequuntur eos magni, tempore, natus sint consequatur officiis qui distinctio dolores? Quasi omnis voluptatibus officiis assumenda. Ratione, animi. Explicabo praesentium culpa voluptas?</span>
+    </div>
+    <?php require 'view/aside.php'; ?>
+  </div>
 </div>
